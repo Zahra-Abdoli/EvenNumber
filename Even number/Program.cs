01e4b5623beffc533e60f01520de5e088985a824
zahra1993a <33681568+zahra1993a@users.noreply.github.com>
2020-05-14 23:23:58 +0200

    avlidation number

--- Even number/Program.cs
+++ Even number/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Even_number
 {
@@ -10,14 +11,40 @@ namespace Even_number
 
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine($"enter your number {i + 1}:");
-                numbers[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"enter your number {i + 1}:  (repetetive number does not count)");
+                try
+                {
+
+                    int search = Convert.ToInt32(Console.ReadLine());
+                    bool IsSame = false;
+
+                    for (int j = 0; j < i; j++)
+                    {
+                        int item = numbers[j];
+                        if (item == search) { IsSame = true; i--; break; }
+
+                    }
+                    if (!IsSame) numbers[i] = search;
+                }
+                catch
+                {
+                    Console.WriteLine("plese enter number");
+                    i--;
+                }
             }
-            foreach(int number in numbers)
+            bool a = false;
+            foreach (int number in numbers)
             {
-                if (number % 2 == 0) Console.WriteLine(number);
+                if (number != 0 && number % 2 == 0)
+                {
+
+                    Console.WriteLine(number);
+                    a = true;
+                }
+                if (number == 0) Console.WriteLine(number);
             }
-;
+            if (!a) Console.WriteLine("non of them");
+            ;
         }
     }
 }
